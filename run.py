6de2726b080c4b0ba886f04ab0f182330eced72c
CyberArchitect777 <169656116+CyberArchitect777@users.noreply.github.com>
2024-07-14 17:51:03 +0100

    Completed the challenge of implementing the sandwich recommendation output

--- run.py
+++ run.py
@@ -123,6 +123,13 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    headings = (SHEET.worksheet("stock").get_all_values())[0]
+    output = {}
+    for i in range(len(headings)):
+        output.update({headings[i]: data[i]})
+    return output
+
 def main():
     """
     Run all program functions
@@ -136,8 +143,11 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 print("Welcome to Love Sandwiches Data Automation")
 get_last_5_entries_sales()
-main()
-
+stock_data = main()
+print("Make the following number of sandwiches for next market:")
+stock_values = get_stock_values(stock_data)
+print(stock_values)
